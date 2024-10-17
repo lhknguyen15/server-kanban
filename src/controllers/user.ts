8d04cea50b8819d6af9ad9eaee15b36f5a40679f
nguyenle <nguyentaro1502@gmail.com>
2024-10-18 02:48:01 +0700

    create a new account

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,10 +1,28 @@
+import UserModel from "../models/UserModel";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+dotenv.config();
+
 const register = async (req: any, res: any) => {
   const body = req.body;
+  const { email, name, password } = body;
+
   try {
-    console.log(body);
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      throw new Error("Account is has already");
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashpassword = await bcrypt.hash(password, salt);
+    console.log(hashpassword);
+
+    const newUser = new UserModel(body)
+    await newUser.save()
+    
+
     res.status(200).json({
       message: "Register",
-      data: body,
+      data: newUser,
     });
   } catch (error: any) {
     res.status(404).json({
