56baf88bc892e6da78f2fe09a43cb24dac73dd8d
nguyenle <nguyentaro1502@gmail.com>
2024-10-18 00:50:10 +0700

    run server

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./src/routers/user";
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const dbURL = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.jccpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is starting at http://localhost:${PORT}`);
-});
+app.use("/auth", userRouter);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbURL);
+    console.log(`Connect DB successfully`);
+  } catch (error) {
+    console.log(`Can not connect DB ${error}`);
+  }
+};
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is starting at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ src/models/UserModel.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserScheme = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updateAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const UserModel = mongoose.model("users", UserScheme);
+export default UserModel;
